c5651a51932791fddae0778e51214a14087110d7
nayana-a11y <nayanahari613o@gmail.com>
2024-11-12 12:22:24 +0530

    Create nayana.cpp

---
+++ phase1/learnings/Day05/nayana.cpp
@@ -0,0 +1,32 @@
+  // sumPrimeAge
+
+    int sumPrimeAge(int [ages], int size){
+        int sum = 0;
+
+        for (i = 0; i < size-1; i++){
+            if (isPrime(ages[i])){
+                sum = sum + ages[i];
+            }
+        }
+        return sum;
+    }
+
+    int main(){
+        sumPrimeAge();
+        return 0;
+    }
+
+
+// function minimumAge
+
+
+    int minAge(int [ages], int size) 
+        int min = 0;
+
+        for i = 0;i < (size-1); i++){
+            if (ages[i] < min){
+                min = ages[i];
+            }
+        }
+        return min;  
+    }
