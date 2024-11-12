7b6462eb0474693b4257a216db625c339b371458
Josh Alex <joshdannyalex@gmail.com>
2024-11-12 13:10:51 +0530

    combined functions

--- phase1/learnings/Day05/test12102024.cpp
+++ phase1/learnings/Day05/test12102024.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 #define MAX_SIZE 100
 using namespace std;
 
@@ -15,7 +16,34 @@ int readAges(int ages[]){
     return index;
 }
 
-int sumOfAge(int ages[], int size){
+int findAvg(int ages[], int size){ 
+    
+    int sumAge = findSumOfAge(ages, size);
+    int avg = sumAge/size;
+    
+    return avg;
+}
+
+bool isPrime(int age){
+    if (age < 2)
+    {
+        return false;
+    }
+
+    int squareRoot = sqrt(age);
+
+    for (int i = 0; i <= squareRoot; i++)
+    {
+        if ((age % i) == 0)
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
+
+int findSumOfAge(int ages[], int size){
         int sum = 0;
 
         for (int i=0; i<size ;i++){
@@ -25,13 +53,93 @@ int sumOfAge(int ages[], int size){
         return sum;
 }
 
-int findAvg(int ages[], int size){ 
-    
-    int sumAge = sumOfAge(ages, size);
-    int avg = sumAge/size;
-    
-    return avg;
+int findSumOfPrime(int [ages], int size){
+    int sum = 0;
+
+    for (i = 0; i < size-1; i++){
+        if (isPrime(ages[i])){
+            sum = sum + ages[i];
+        }
+    }
+    return sum;
 }
+
+bool isTeenage(int age){
+
+    if ((age >= 13) && (age <= 19)){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int findSumOfTeenage(int ages[] , int size){
+    int sumOfTeenage = 0;
+    for(int i=0 ; i < size ; i++)
+    {
+        if(isTeenage(ages[i]))
+        {
+            sumOfTeenage = sumOfTeenage+ages[i];
+        }
+    }
+    return sumOfTeenage;
+}
+
+int findMinAge(int ages[], int size){
+    int min = 0;
+
+    for (int i = 0; i < (size-1); i++){
+        if (ages[i] < min){
+            min = ages[i];
+        }
+    }
+    return min;  
+}
+
+int sumOfOddAge(int ages[], int size){
+    int sum = 0;
+
+    for(int i = 0; i < size; i++){
+        if (isOddAge(ages[i])){
+            sum = sum + ages[i];
+        }
+    }
+    return sum;
+}
+
+bool isOddAge(int age){
+        
+    if ((age % 2) == 0){         
+        return false;
+    }
+    return true;
+}
+
+int findSecondMaxAdultAge(int ages[], int size){
+    int firstMax = 0, secondMax = 0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (ages[i] >= 18 && ages[i] > firstMax)
+        {
+            firstMax = ages[i];
+        }
+    }
+    
+    for (int i = 0; i < size; i++)
+    {
+        if ((ages[i] >= 18) && (ages[i] > secondMax) && (ages[i] < firstMax))
+        {
+            secondMax = ages[i];
+        }
+    }	
+    
+    return secondMax;
+}
+
+
+
 
 void functionTest(){
     
@@ -40,9 +148,30 @@ void functionTest(){
     int size = readAges(ages);
     
     int avgOfAge = findAvg(ages, size);
-    
     cout << "Average of input age: " << avgOfAge << endl;
     
+    int primeAgeSum = findSumOfPrime(ages, size);
+    cout << "Sum of prime age: " << primeAgeSum << endl;
+    
+    int teenSum = findSumOfTeenage(ages, size);
+    cout << "Sum of teenagers age: " << teenSum << endl;
+    
+    int minimumAge = findMinAge(ages, size);
+    cout << "Minimum age: " << minimumAge << endl;
+    
+    int oddAgeSum = sumOfOddAge(ages, size);
+    cout << "Sum of odd age: " << oddAgeSum << endl;
+    
+    int secondMaxAdultAge = findSecondMaxAdultAge(ages, size);
+    cout << "Second maximum adult age: " << secondMaxAdultAge << endl;
+    
+    if(isPrime(minimumAge)){
+        cout << "Minimum age is prime" << endl;
+    }
+    else{
+        cout << "Minimum age is not prime" << endl;
+        
+    }
 }
 
 
